286492d907e61bd17e9c06453169c8ce819c1c04
hellomatia <shuaigejp80@gmail.com>
2024-08-19 20:44:17 +0900

    [init] 프로젝트 초기화

---
+++ src/test/java/woowa/team4/redis/study/RedisStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package woowa.team4.redis.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
